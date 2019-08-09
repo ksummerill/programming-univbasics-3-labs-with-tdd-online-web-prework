5ac556c7e1ab08c0be8eb295d1d0d56e88356944
Kaitlyn Summerill <kaitlynsummerill@gmail.com>
2019-08-09 20:41:14 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,4 +3,4 @@
 first_number = 5
 second_number = 75
 sum = first_number + second_number
-difference = second_number - first_number || first_number - second_number
+difference = first_number-second_number
